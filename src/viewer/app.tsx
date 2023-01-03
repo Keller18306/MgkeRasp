53cdecf435753a5b91a7489b1fc6846541eae7e1
Keller <kompeter17@gmail.com>
2023-01-03 11:55:08 +0300

    group search

--- src/viewer/app.tsx
+++ src/viewer/app.tsx
@@ -1,15 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Groups } from "../parser/types";
 import GroupBlock from "./group";
+import Search from "./search";
 
 export default function ViewerApp({ groups }: { groups: Groups }) {
+    const [searchValue, setSearchValue] = useState<string | undefined>()
+
+    useEffect(() => {
+        const groupValue = localStorage.getItem('mgke-rasp_group')
+
+        if (groupValue) {
+            setSearchValue(groupValue)
+        }
+    }, []);
+
     const blocks: JSX.Element[] = [];
 
     for (const groupNumber in groups) {
+        if (searchValue && groupNumber !== searchValue) continue;
+
         blocks.push(<GroupBlock groupNumber={groupNumber} group={groups[groupNumber]} />)
     }
 
     return <React.Fragment>
+        <Search searchValue={searchValue} setSearch={setSearchValue} />
         {blocks}
     </React.Fragment>;
 }
---
+++ src/viewer/search.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function ViewerSearch({ searchValue, setSearch }: { searchValue: string | undefined, setSearch: React.Dispatch<React.SetStateAction<string | undefined>> }) {
+    return <div className="searchBlock">
+        <input type="text" value={searchValue} placeholder="Группа" onChange={(e) => {
+            setSearch(e.target.value);
+        }} />
+
+        <input type="button" value="Сохранить" onClick={() => {
+            if (searchValue) {
+                localStorage.setItem('mgke-rasp_group', searchValue)
+            } else {
+                localStorage.removeItem('mgke-rasp_group')
+            }
+        }} />
+    </div>
+}
